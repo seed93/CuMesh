2ca6a18dad3cb0f2368d7fc844d79cfdf6cbac3a
liangd <liangding1990@163.com>
2026-02-04 23:33:18 +0800

    remove import unused func

--- cumesh/__init__.py
+++ cumesh/__init__.py
@@ -1,4 +1,4 @@
-from . import remeshing
+# from . import remeshing
 from .cumesh import CuMesh
-from .bvh import cuBVH
-from .xatlas import Atlas
+# from .bvh import cuBVH
+# from .xatlas import Atlas
